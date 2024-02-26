d6a30466aa6ee6c5388af13638cd0a9f1df58ea6
Brenda Wihogora <brewihogora23@gmail.com>
2024-02-26 21:53:47 +0200

    Add save user method.

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -1,4 +1,32 @@
 package dao;
 
+
+import model.User;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import utils.HibernateUtil;
+
+//CRUD operations
 public class UserDao {
+//    save user;
+//    @param user
+
+    public void saveUser(User user){
+        Transaction transaction = null;
+        try(Session session = HibernateUtil.getSessionFactory().openSession()){
+            //start transaction
+            transaction = session.beginTransaction();
+            //save student object
+            session.save(user);
+            // commit trans
+            transaction.commit();
+        }catch (Exception e){
+            if (transaction != null){
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+
+
+    }
 }
